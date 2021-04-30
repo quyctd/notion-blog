e9617b9106bdfd888f760f232170d6456882636d
Quý Đinh <quy.dc98@gmail.com>
2021-04-30 22:44:29 +0700

    Re-fork notion-blog

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import ExtLink from './ext-link'
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <span>
+          Made with ❤️ - Powered by{' '}
+          <ExtLink href="https://github.com/ijjk/notion-blog">
+            Notion-blog
+          </ExtLink>{' '}
+          ⚡️
+        </span>
+      </footer>
+    </>
+  )
+}
