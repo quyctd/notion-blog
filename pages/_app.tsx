c9fc9f8412b3046a43cbf02e26a4c9026fbad9bf
quyctd <quy.dc98@gmail.com>
2021-08-17 23:14:21 +0700

    Some how rendered the post

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from "next/app"
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components"
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css"
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css"
+
+// used for collection views (optional)
+import "rc-dropdown/assets/index.css"
+
+// used for rendering equations (optional)
+import "katex/dist/katex.min.css"
 
 const GlobalStyle = createGlobalStyle`
   html {
---
+++ pages/post/[pageId].tsx
@@ -0,0 +1,76 @@
+import Head from "next/head"
+
+import { getPageTitle, getAllPagesInSpace } from "notion-utils"
+import { NotionAPI } from "notion-client"
+import { NotionRenderer, Code, CollectionRow } from "react-notion-x"
+
+const notion = new NotionAPI()
+
+export const getStaticProps = async (context: any) => {
+  const pageId = context.params.pageId as string
+  const recordMap = await notion.getPage(pageId)
+
+  return {
+    props: {
+      recordMap,
+    },
+    revalidate: 10,
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  }
+
+  const rootNotionPageId = "067dd719a912471ea9a3ac10710e7fdf"
+  const rootNotionSpaceId = "fde5ac74-eea3-4527-8f00-4482710e1af3"
+
+  // This crawls all public pages starting from the given root page in order
+  // for next.js to pre-generate all pages via static site generation (SSG).
+  // This is a useful optimization but not necessary; you could just as easily
+  // set paths to an empty array to not pre-generate any pages at build time.
+  const pages = await getAllPagesInSpace(
+    rootNotionPageId,
+    rootNotionSpaceId,
+    notion.getPage.bind(notion),
+    {
+      traverseCollections: false,
+    }
+  )
+
+  const paths = Object.keys(pages).map((pageId) => `/${pageId}`)
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export default function NotionPage({ recordMap }: any) {
+  if (!recordMap) {
+    return null
+  }
+
+  const title = getPageTitle(recordMap)
+
+  return (
+    <>
+      <Head>
+        <meta name="description" content="React Notion X demo renderer." />
+        <title>{title}</title>
+      </Head>
+
+      <NotionRenderer
+        components={{
+          code: Code,
+          collectionRow: CollectionRow,
+        }}
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+      />
+    </>
+  )
+}
